5f2a7691715ec6f1fd6a20db2746ee0ff97891c3
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-02-18 22:07:06 -0600

    UI Setup

---
+++ Assets/Scripts/HelloScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Utils;
+
+public class HelloScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var result = ArraySorter.MedirInsertionSort(1000);
+        Debug.Log($"El algoritmo duro {result} milisegundos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utils/ArraySorter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Utils
+{
+    class ArraySorter
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static int MedirInsertionSort(int numeroDeElementos)
+        {
+            // Crear arreglo con la cantidad de elementos
+            int[] arregloAleatorio = GenerarRandomArray(numeroDeElementos);
+            // Empezar a medir el tiempo
+            stopwatch.Start();
+            InsertionSort(arregloAleatorio);
+            stopwatch.Stop();
+            return stopwatch.Elapsed.Milliseconds;
+        }
+
+        public static void MedirStupidSort(int numeroDeElemento)
+        {
+            // TODO: CARLOS
+        }
+
+        // Generar arreglo de numeros enteros aleatorios
+        public static int[] GenerarRandomArray(int numeroDeElementos)
+        {
+            int[] randomArr = new int[numeroDeElementos];
+            Random randonNum = new Random();
+            // Rellenar arreglo con numeros aleatorios
+            for (int i = 0; i < randomArr.Length; i++)
+            {
+                randomArr[i] = randonNum.Next(numeroDeElementos);
+            }
+            return randomArr;
+        }
+        // Algoritmo de ordenamiento Insertion Sort
+        public static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                // Almacenar valor actual
+                int valorActual = arr[i];
+                // Crear puntero
+                int pointer = i - 1;
+                while (pointer >= 0 && arr[pointer] > valorActual)
+                {
+                    arr[pointer + 1] = arr[pointer];
+                    pointer -= 1;
+                }
+                arr[pointer + 1] = valorActual;
+            }
+        }
+    }
+}
