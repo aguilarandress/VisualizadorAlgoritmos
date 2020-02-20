473f907382434965b8992f45500e72af1d8fc830
Titovare <carlosjvare@gmail.com>
2020-02-20 11:40:49 -0600

    Versión que coloca puntos

--- Assets/Scripts/HelloScript.cs
+++ Assets/Scripts/HelloScript.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
-
 using Utils;
 
 public class HelloScript : MonoBehaviour
 {
+    [SerializeField] private Sprite circuloSprite;
+    public RectTransform graphContainer;
     // Start is called before the first frame update
     void Start()
     {
-        var result = ArraySorter.MedirInsertionSort(1000);
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+
+        var result = ArraySorter.MedirInsertionSort(5000);
         Debug.Log($"El algoritmo duro {result} milisegundos");
-        result = ArraySorter.MedirMergeSort(1000);
+        crearCirculo(new Vector2(600, result));
+        result = ArraySorter.MedirMergeSort(5000);
         Debug.Log($"El algoritmo duro {result} milisegundos");
+        crearCirculo(new Vector2(600, result));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void crearCirculo(Vector2 position)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circuloSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = position;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+
     }
 }
